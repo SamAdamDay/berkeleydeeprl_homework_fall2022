dd98a55b0d4899478215579a4af386cafe3e119c
Sam Adam-Day <me@samadamday.com>
2023-03-15 12:36:56 +0000

    Implemented sample_random_data method

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -79,12 +79,17 @@ class ReplayBuffer(object):
             == self.terminals.shape[0]
         )
 
-        ## TODO return batch_size number of random entries from each of the 5 component arrays above
-        ## HINT 1: use np.random.permutation to sample random indices
-        ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
-        ## HINT 3: look at the sample_recent_data function below
+        permutor = np.random.permutation(self.obs.shape[0])
 
-        return TODO, TODO, TODO, TODO, TODO
+        ## Return batch_size number of random entries from each of the 5 component arrays above
+
+        return (
+            self.obs[permutor],
+            self.acs[permutor],
+            self.rews[permutor],
+            self.next_obs[permutor],
+            self.terminals[permutor],
+        )
 
     def sample_recent_data(
         self, batch_size: int = 1
