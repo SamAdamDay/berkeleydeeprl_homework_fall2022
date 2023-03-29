e9d9b9cfcb6615dd384e5dd707682333dc4164de
Sam Adam-Day <me@samadamday.com>
2023-03-29 15:44:46 +0100

    Updated hw2_q2 script parameter values

--- hw2/cs285/scripts/run_hw2_q2.py
+++ hw2/cs285/scripts/run_hw2_q2.py
@@ -5,9 +5,10 @@ import itertools
 SCRIPT_PATH = os.path.realpath(__file__)
 RUN_SCRIPT_PATH = os.path.join(os.path.dirname(SCRIPT_PATH), "run_hw2.py")
 
-LR_VALUES = [0.1, 0.01, 0.001, 0.0001, 0.00001]
-BATCH_SIZE_VALUES = [10, 50, 100, 500, 1000, 5000]
-SEED_VALUES = [8859]
+LR_VALUES = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+BATCH_SIZE_VALUES = [500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000]
+# SEED_VALUES = [8859, 5543, 1853, 2445]
+SEED_VALUES = [5543, 1853, 2445]
 
 parameter_grid = list(itertools.product(LR_VALUES, BATCH_SIZE_VALUES, SEED_VALUES))
 for i, (lr, batch_size, seed) in enumerate(parameter_grid):
