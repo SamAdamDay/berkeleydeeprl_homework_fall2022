2254741db72ca8c0fbc08b68675b6b736bac0b8f
Sam Adam-Day <me@samadamday.com>
2023-05-11 12:39:30 +0100

    Added Gaussian smoothing to hw2q5 plot

--- hw2/report/scripts/make_q5_graphs.py
+++ hw2/report/scripts/make_q5_graphs.py
@@ -1,8 +1,9 @@
 import os
 import re
-from collections import OrderedDict
 
 import numpy as np
+
+from scipy.ndimage import gaussian_filter1d
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -11,13 +12,17 @@ from tensorboard.backend.event_processing.event_accumulator import EventAccumula
 
 SCRIPT_PATH = os.path.realpath(__file__)
 DATA_DIR = os.path.normpath(SCRIPT_PATH + "/../../../data")
-EXPERIMENT_NAME_REGEX = re.compile(r"q5_lr0.001+_bs2000+_lambda([0-9.]+)_HalfCheetah-v4")
+EXPERIMENT_NAME_REGEX = re.compile(r"q5_lr0.001_bs2000_lambda([0-9.]+)_Hopper-v4")
+PLOT_SMOOTHING = 0
 GRAPH_TITLE = "Training curve for Hopper-v4 using generalised advantage estimation for different lambdas"
+if PLOT_SMOOTHING > 0:
+    GRAPH_TITLE += f" with Gaussian smoothing sigma={PLOT_SMOOTHING}"
 COLOURMAP = "Set1"
+LIGHT_COLOUR_ALPHA = 0.3
 
 print("Loading log data...")
 
-# A list of elements `(gae_lambda, Eval_AverageReturn)`
+# A list of elements `(gae_lambda, avg_return, std_return)`
 experiment_logs = []
 
 for filename in os.listdir(DATA_DIR):
@@ -34,15 +39,26 @@ for filename in os.listdir(DATA_DIR):
     # Load the logs
     event_acc = EventAccumulator(filepath)
     event_acc.Reload()
-    event_list = event_acc.Scalars("Eval_AverageReturn")
-    curve = []
-    for i, event in enumerate(event_list):
-        assert i == event.step
-        curve.append(event.value)
-    experiment_logs.append((gae_lambda, np.array(curve)))
+    avg_events = event_acc.Scalars("Eval_AverageReturn")
+    std_events = event_acc.Scalars("Eval_StdReturn")
+    avg_return = np.zeros(len(avg_events))
+    std_return = np.zeros(len(std_events))
+    for i, (avg_event, std_event) in enumerate(zip(avg_events, std_events)):
+        assert i == avg_event.step == std_event.step
+        avg_return[i] = avg_event.value
+        std_return[i] = std_event.value
+    experiment_logs.append((gae_lambda, avg_return, std_return))
+
+print("Processing data...")
 
 # Sort the logs according to lambda
 experiment_logs.sort(key=lambda t: t[0])
+
+if PLOT_SMOOTHING > 0:
+    for i, (gae_lambda, avg_return, std_return) in enumerate(experiment_logs):
+        avg_return = gaussian_filter1d(avg_return, sigma=PLOT_SMOOTHING)
+        std_return = gaussian_filter1d(std_return, sigma=PLOT_SMOOTHING)
+        experiment_logs[i] = (gae_lambda, avg_return, std_return)
 
 print("Plotting graph...")
 
@@ -50,9 +66,15 @@ cmap = mpl.colormaps[COLOURMAP]
 
 fig, ax = plt.subplots(1, 1, figsize=(15, 10))
 
-for i, (gae_lambda, avg_return) in enumerate(experiment_logs):
+for i, (gae_lambda, avg_return, std_return) in enumerate(experiment_logs):
     label = f"Lambda {gae_lambda}"
     x_values = np.arange(avg_return.shape[0])
+    ax.fill_between(
+        x_values,
+        y1=avg_return - std_return,
+        y2=avg_return + std_return,
+        color=cmap(i, alpha=LIGHT_COLOUR_ALPHA),
+    )
     ax.plot(x_values, avg_return, color=cmap(i), label=label)
 
 ax.set(xlabel="Iteration", ylabel="Return", title=GRAPH_TITLE)
